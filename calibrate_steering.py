179ea2ab6461c04044db5f4b38554b1c542c18d7
Nicole Fronda <nfronda@gmail.com>
2021-04-07 20:36:31 +0100

    adding gitignore, modifying calibrate_steering script

--- calibrate_steering.py
+++ calibrate_steering.py
@@ -1,6 +1,11 @@
-from picarx_improved import forward
+from ezblock import __reset_mcu__
 import time
+__reset_mcu__()
+time.sleep(0.01)
+import picarx_improved as pi
 
+CALIBRATION_ANGLE = 2  # MODIFY THIS UNTIL CAR DRIVES STRAIGHT
+pi.dir_servo_angle_calibration(CALIBRATION_ANGLE)
 if __name__ == "__main__":
-    forward(50)
+    pi.forward(50)
     time.sleep(2)  # seconds
