33766bcca39306e795ec8ffd45784f72165d6536
Nicole Fronda <nfronda@gmail.com>
2021-04-07 12:39:04 -0700

    adding additional calibration tests in calibrate_steering

--- calibrate_steering.py
+++ calibrate_steering.py
@@ -1,11 +1,16 @@
-from ezblock import __reset_mcu__
+from ezblock import __reset_mcu__, delay
 import time
 __reset_mcu__()
 time.sleep(0.01)
 import picarx_improved as pi
 
 CALIBRATION_ANGLE = 2  # MODIFY THIS UNTIL CAR DRIVES STRAIGHT
+TEST_TURN_ANGLE = 30
 pi.dir_servo_angle_calibration(CALIBRATION_ANGLE)
 if __name__ == "__main__":
     pi.forward(50)
-    time.sleep(2)  # seconds
+    delay(1000)
+    pi.set_dir_servo_angle(TEST_TURN_ANGLE)
+    delay(1000)
+    pi.forward(50, turn_angle=TEST_TURN_ANGLE)
+    delay(1000)
