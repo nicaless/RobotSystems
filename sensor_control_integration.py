7ffbeda11f4a6fc48be0459dc49d5b1d49ffe0a3
Nicole Fronda <nfronda@gmail.com>
2021-04-16 12:30:25 -0700

    adding tests to sensor_control_integration

--- sensor_control_integration.py
+++ sensor_control_integration.py
@@ -14,3 +14,30 @@ def move_forward(sensitivity=50, polarity=1, target=300, speed=20, delay=1000):
         steer_angle = controller.turn_to_line(rel_line_pos)
         pi.forward(speed, turn_angle=steer_angle)
         pi.delay(delay)
+
+
+def test_sensor(sensor):
+    for i in range(50):
+        print(sensor.get_adc_values())
+
+def test_interpreter(sensor, interpreter):
+    for i in range(50):
+        adc_values = sensor.get_adc_values()
+        print(interpreter._find_target(adc_values))
+        print(interpreter.relative_line_position(adc_values))
+
+def test_controller(sensor, interpreter, controller):
+    rel_line_pos = interpreter.relative_line_position(sensor.get_adc_values())
+    controller.turn_to_line(rel_line_pos)
+
+
+if __name__ == "__main__":
+    pi = PiCarX()
+    sensor = Sensor()
+    interpreter = Interpreter()
+    controller = Controller(pi)
+
+    test_sensor(sensor)
+    test_interpreter(sensor, interpreter)
+    test_controller(sensor, interpreter, controller)
+
