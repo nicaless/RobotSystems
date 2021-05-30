f7748e64881e92f2bbf0b09949e6eab0c7458efc
Nicole Fronda <nfronda@gmail.com>
2021-05-29 19:54:05 -0700

    threading using multiprocessing library

--- arm/PianoPlayer.py
+++ arm/PianoPlayer.py
@@ -3,82 +3,118 @@
 import sys
 sys.path.append('/home/pi/ArmPi/')
 from ArmController import ArmController
+from ArmIK.ArmMoveIK import ArmIK
 import cv2
 import HiwonderSDK.Board as Board
-import logging
+from multiprocessing import Process, Queue
+from one_item_queue import OneItemQueue
 from PianoTracker import PianoTracker
-from rossros import Bus, Consumer, Producer, Timer
-from rossros import runConcurrently
 import time
 
-if sys.version_info.major == 2:
-    print('Please run this program with python3!')
-    sys.exit(0)
 
-DEBUG = logging.DEBUG
-logging_format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=logging_format, level=logging.INFO,
-                    datefmt ="%H:%M:%S")
+ESCAPE_KEY = 27
 
 
-tracker = PianoTracker()
+def user_input(tracker, input_queue, end_queue):
+    while True:
+        try:
+            end = end_queue.get_nowait()
+            if end == 'END':
+                break
+        except:
+            continue
 
-### SET UP TIMER
-timer_bus = Bus(name='timer bus')
-timer = Timer(timer_bus, delay=5)
+        print('Available Notes: ')
+        print(tracker.key_map.values())
 
+        note = input('Enter note(s) to play: ')
 
-### THREAD FOR GETTING IMAGES
-img_bus = Bus(name='Image Bus')
-img_producer = Producer(tracker.get_frame, img_bus,
-                        delay=0, termination_busses=[timer_bus])
+        input_queue.put({'play': note})
 
-### THREAD FOR DISPLAYING IMAGES
-def display(img):
-    if img is None:
-        print('no image')
-    cv2.imshow('Keyboard', img)
-    cv2.waitKey(1)
-    
-img_consumer = Consumer(display, img_bus,
-                        delay=1, termination_busses=[timer_bus])
-
-note_bus = Bus(name='Note Bus')
-def get_notes(tracker):
-    print('Available Notes: ')
-    print(tracker.key_map.values())
-
-    note = input('Enter note(s) to play: ')
-
-    return note
+    input_queue.put('END')
 
 
-def display_key(tracker, note):
-    img = tracker.latest_img
-    box, coords = tracker.get_key_pos(note)
+def display(tracker, end_queue):
+    while True:
+        img = tracker.get_frame()
+        if img is not None:
+            cv2.imshow('Keyboard', img)
+            key = cv2.waitKey(1)
 
-    # TEST DRAW BOX
-    cv2.drawContours(img, [box], -1, (0, 0, 200), 2)
-    cv2.imshow('Align Frame', img)
-    cv2.putText(img, '(' + str(coords[0]) + ',' + str(coords[1]) + ')',
-                (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 200), 1)
+            if key == ESCAPE_KEY:
+                break
+
+    end_queue.put('END')
+    tracker.camera_close()
+    cv2.destroyAllWindows()
 
 
-### FULL THREAD LIST
-thread_list = [timer, img_producer, img_consumer]
+def process_note(tracker, input_queue, coords_queue):
+    while True:
+        if input_queue.empty():
+            continue
+        note = input_queue.get()
+        if note == 'END':
+            break
+
+        box, coords = tracker.get_key_pos(note)
+        coords_queue.put({'coords': coords})
+
+    coords_queue.put('END')
+
+
+
+def play(controller, coords_queue):
+    while True:
+        if coords_queue.empty():
+            continue
+        coords = coords_queue.get()
+        if coords == 'END':
+            break
+
+        controller.play_key(coords['coords'])
+
+    controller.initial_position()
+
+
+def print(queue):
+    while True:
+        if queue.empty():
+            continue
+        item = queue.get()
+        print(item)
 
 
 if __name__ == '__main__':
+    arm = ArmIK()
+    controller = ArmController(arm, Board)
+    controller.close(full=True)
+
+    tracker = PianoTracker()
     tracker.calibrate()
+
     tracker.camera_open()
-#     time.sleep(1)
-#     img = tracker.get_frame()
-#     display(img)
-#     time.sleep(1)
-    runConcurrently(thread_list)
-    tracker.camera_close()
-    cv2.destroyAllWindows()
+
+    end_queue = OneItemQueue()
+    input_queue = Queue()
+    coords_queue = Queue()
+
+    # p1 = Process(target=user_input,
+    #              args=(tracker, input_queue, end_queue))
+    p1 = Process(target=display,
+                 args=(tracker, end_queue))
+    p2 = Process(target=process_note,
+                 args=(tracker, input_queue, coords_queue))
+    p3 = Process(target=play,
+                 args=(controller, coords_queue))
+    p4 = Process(target=print,
+                 args=(input_queue))
+
+    p1.start()
+    p4.start()
+
+    user_input(tracker, input_queue, end_queue)
+
 
 
 
